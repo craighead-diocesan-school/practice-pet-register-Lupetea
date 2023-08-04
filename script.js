768d62cc54703e92d72e0529a97fe807f6de8e8f
Lupetea <124636830+Lupetea@users.noreply.github.com>
2023-08-04 13:59:48 +1200

    improved code

--- script.js
+++ script.js
@@ -1,39 +1,54 @@
-let animalName = [
+let pets = [
   { name: "Blinkey", age: 5, species: "sheep" },
   { name: "Piper", age: 6, species: "Dog" },
   { name: "Lola", age: 7, species: "cat" },
   { name: "Rudy", age: 3, species: "rabbit" },
 ]
 
+function showPet() {
+  for (let pet of pets) {
+    alert(pet.name)
+  }
+}
+
 function addName() {
-  let name = prompt("Give me a  name")
-  let age = prompt("What is the age of your pet")
-  let species = prompt("What is the age of your pet?")
-  let pet = {
+  let name = prompt("Pet name?")
+  let age = prompt("Pets age?")
+  let species = prompt("Pet species?")
+
+  //create a pet with the details
+  let pets = {
     name: name,
     age: age,
     species: species,
   }
-  animalName.push(newName)
 }
-function findPet() {
-  let searchtarget = prompt("Which pet do you want to find?")
-  let searchResult = false
-
-  for (let currentNumber of animal) {
-    if (currenumber === Number(searchAnimal)) searchResult = true
-  }
-}
-
-if ((searchResult = true)) {
-  alert(searchAnimal + "was found")
-} else {
-  alert(searchAnimal + "was not found")
-}
-
-alert(searchAnimal + "found:" + searchResult)
+pets.push(pet)
 
 function removeName() {
-  let index = prompt("What name do you want to remove?")
-  animalName.splice(removeName)
+  let index = 0
+  for (let pet of pets) {
+    alert(index + ":" + pet.name)
+  }
+}
+let indextoRemove = prompt("What pet do you want ot remove?")
+
+pets.splice(indextoRemove, 1)
+
+function searchPet() {
+  let petToSearchFor = "Which pet do you wish to find?"
+
+  let searchResult = false
+
+  for (let pet of pets) {
+    if (pet.name == petToSearchFor) {
+      searchResult = true
+    }
+  }
+
+  if ((searchResult = true)) {
+    alert("Found the pet!")
+  } else {
+    alert("Did not find pet!")
+  }
 }
